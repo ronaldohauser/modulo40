1892c6a3987f94bdcb803c7d4d97d04c90206ffc
Ronaldo Hauser Dias <duqueronaldo@icloud.com>
2024-06-18 10:54:06 -0300

    Entrega modulo 40 - Spring Boot

---
+++ OneDrive/Área de Trabalho/EBAC/Desenvolvedor Full Stack Java/Backend/modulo40/src/main/java/com/rdias/modulo40/Modulo40Application.java
@@ -0,0 +1,13 @@
+package com.rdias.modulo40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo40Application.class, args);
+	}
+
+}
---
+++ OneDrive/Área de Trabalho/EBAC/Desenvolvedor Full Stack Java/Backend/modulo40/src/test/java/com/rdias/modulo40/Modulo40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdias.modulo40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
